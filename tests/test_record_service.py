33f2cd7d27b6c76a9a0922fd7b2cf1f0e5972dbb
LittleLittleGull <3259508730@qq.com>
2025-05-12 13:29:55 +0800

    添加了对 RecordService 的单元测试

--- tests/test_record_service.py
+++ tests/test_record_service.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 import unittest
 
 from src.model.exercise import Exercise
@@ -40,61 +41,31 @@ class TestRecordService(unittest.TestCase):
         custom_track="",
     )  # User在本测试中没有影响
     service = RecordService(None, exercise, route, user)
-    start_record_expected = {
-        "routeName": "Test田径场",
-        "ruleId": "25",
-        "planId": "10",
-        "recordTime": "2025-01-01",
-        "startTime": "19:00:00",
-        "startImage": "A.jpg",
-        "endTime": "",
-        "exerciseTimes": "",
-        "routeKilometre": "",
-        "endImage": "",
-        "strLatitudeLongitude": [],
-        "routeRule": "Test校区",
-        "maxTime": 12,
-        "minTime": 4,
-        "orouteKilometre": 1.2,
-        "ruleEndTime": "22:00",
-        "ruleStartTime": "06:00",
-        "calorie": 0,
-        "speed": "0'00''",
-        "dispTimeText": 0,
-        "studentId": "123",
-    }
-    finish_record_expected = {
-        "routeName": "Test田径场",
-        "ruleId": "25",
-        "planId": "10",
-        "recordTime": "2025-01-01",
-        "startTime": "19:00:00",
-        "startImage": "A.jpg",
-        "endTime": "19:10:00",
-        "exerciseTimes": 600,
-        "routeKilometre": "1.00",
-        "endImage": "B.jpg",
-        "strLatitudeLongitude": "[Track]",
-        "routeRule": "Test校区",
-        "maxTime": 12,
-        "minTime": 4,
-        "orouteKilometre": 1.2,
-        "ruleEndTime": "22:00",
-        "ruleStartTime": "06:00",
-        "calorie": "62",
-        "speed": "10'00''",
-        "dispTimeText": "00:10:00",
-        "studentId": "123",
-        "id": "123",
-        "nowStatus": 2,
-    }
+    start_record_expected = (
+        '{"routeName": "Test田径场", "ruleId": "25", "planId": "10", "recordTime": "2025-01-01",'
+        ' "startTime": "19:00:00", "startImage": "A.jpg", "endTime": "", "exerciseTimes": "",'
+        ' "routeKilometre": "", "endImage": "", "strLatitudeLongitude": [], "routeRule": "Test校区",'
+        ' "maxTime": 12, "minTime": 4, "orouteKilometre": 1.2, "ruleEndTime": "22:00",'
+        ' "ruleStartTime": "06:00", "calorie": 0, "speed": "0\'00\'\'", "dispTimeText": 0, "studentId": "123"}'
+    )
+    finish_record_expected = (
+        '{"routeName": "Test田径场", "ruleId": "25", "planId": "10", '
+        '"recordTime": "2025-01-01", "startTime": "19:00:00", "startImage": "A.jpg", '
+        '"endTime": "19:10:00", "exerciseTimes": 600, "routeKilometre": "1.00", '
+        '"endImage": "B.jpg", "strLatitudeLongitude": "[Track]", "routeRule": "Test校区", '
+        '"maxTime": 12, "minTime": 4, "orouteKilometre": 1.2, "ruleEndTime": "22:00", '
+        '"ruleStartTime": "06:00", "calorie": "62", "speed": "10\'00\'\'", '
+        '"dispTimeText": "00:10:00", "studentId": "123", "id": "123", "nowStatus": 2}'
+    )
 
-    def test_start_record(self):
+    def test_get_start_record(self):
         start_record = self.service.get_start_record("A.jpg")
+        start_record = json.dumps(start_record, ensure_ascii=False)
         self.assertEqual(self.start_record_expected, start_record)
 
     def test_get_finish_record(self):
         finish_record = self.service.get_finish_record("A.jpg", "B.jpg", "123")
+        finish_record = json.dumps(finish_record, ensure_ascii=False)
         self.assertEqual(self.finish_record_expected, finish_record)
 
 
