efb71109636178341b69b7ea9104a3fac64e3d84
leostudiooo <lilingfeng0408@outlook.com>
2025-05-14 21:50:47 +0800

    重命名 GOOSEApp 文件并更新导入路径

--- GOOSE.py
+++ GOOSE.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from src.ui.tui.App import GOOSEApp
+from src.ui.tui.GOOSEApp import GOOSEApp
 
 if __name__ == "__main__":
 	app = GOOSEApp()
